6522a2e239ca530fa2402297267bae114eb8d2a0
rex-cwyu <rex.cwyu@gmail.com>
2018-06-09 20:30:19 +0800

    Learn Redux #15 — Updating Comment State in our Store

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -12,11 +12,18 @@ const Comments = React.createClass({
       </div>
     );
   },
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  },
   render() {
     return (
       <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
-        <form className='comment-form' ref='commentForm'>
+        <form className='comment-form' ref='commentForm' onSubmit={this.handleSubmit}>
           <input type='text' ref='author' placeholder='author' />
           <input type='text' ref='comment' placeholder='comment' />
           <input type='submit' hidden />
