f38bff3c193db371d9fdf8f061bb656a0e1e6bc0
rex-cwyu <rex.cwyu@gmail.com>
2018-06-10 17:40:14 +0800

    Learn Redux #19 — Redux Dev Tools

--- client/store.js
+++ client/store.js
@@ -14,9 +14,14 @@ const defaultState = {
   comments,
 };
 
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 const store = createStore(
   rootReducer,
   defaultState,
+  enhancers,
 );
 
 export const history = syncHistoryWithStore(browserHistory, store);
